7a043d94da34227323e9fb5402a15554cffe73e1
Gauravpurohit409 <gauravpurohit409@gmail.com>
2023-02-22 02:10:28 +0530

    Add routes and modify development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -59,7 +59,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  config.action_mailer.default_url_options = {:host => "http://127.0.0.1", :port => '3000', :protocol => "http"}
+  config.action_mailer.preview_path = "#{Rails.root}/tmp/mailers/previews"
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  scope :api, defaults: { format: :json } do
+    mount_devise_token_auth_for 'User', skip: [:invitations], controllers: { 
+      # sessions: :sessions, 
+      # registrations: "registrations", 
+      confirmations: :confirmations
+    },
+    path_names: { sign_in: :login, sign_out: :logout }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    devise_for :user, class_name: "User", only: [:invitations], controllers: {invitations: "users/invitations"}
+    resource :referrals, only: [:show] do
+      get :send_invite
+    end
+  end
+
 end
